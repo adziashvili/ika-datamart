d55b246087117c25757b16f26e791a6a2202895e
Adziashvili <itzik@186590d20f27.ant.amazon.com>
2018-09-30 14:45:59 +0800

    Initial version

---
+++ source/datamart/record.js
@@ -0,0 +1,13 @@
+    export default class Record {
+      exist(key) {
+        return Object.keys(this).includes(key)
+      }
+
+      set(key, value) {
+        this[key] = value
+      }
+
+      get(key) {
+        return this[key]
+      }
+    }
---
+++ source/index.js
@@ -0,0 +1,13 @@
+    import Analyzer from './datamart/analyzer'
+    import Dictionary from './datamart/dictionary'
+    import Record from './datamart/record'
+    import Table from './datamart/table'
+    import BaseExcelParser from './datamart/baseExcelParser'
+
+    export {
+      Analyzer,
+      Dictionary,
+      Record,
+      Table,
+      BaseExcelParser
+    }
